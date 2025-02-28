2ee31e70613c10f4e315eed601d396ef9abce7e7
Lê Chí Hiếu <76096295+hieuLapTop77@users.noreply.github.com>
2025-02-28 09:54:33 +0700

    Update TickNet.py

--- models/TickNet.py
+++ models/TickNet.py
@@ -17,19 +17,20 @@ class FR_PDP_block(torch.nn.Module):
         super().__init__()
         self.Pw1 = conv1x1_block(in_channels=in_channels,
                                 out_channels=in_channels,                                
-                                use_bn=False,
-                                activation=None)
+                                use_bn=True,
+                                activation='relu')
         self.Dw = conv3x3_dw_blockAll(channels=in_channels, stride=stride)         
         self.Pw2 = conv1x1_block(in_channels=in_channels,
                                              out_channels=out_channels,                                             
-                                             groups=1)
+                                             groups=1, use_bn=True, activation="relu")
         self.PwR = conv1x1_block(in_channels=in_channels,
                                 out_channels=out_channels,
-                                stride=stride)
+                                stride=stride, use_bn=True, activation="relu")
         self.stride = stride
         self.in_channels = in_channels
         self.out_channels = out_channels
-        self.SE = SE(out_channels, 16)
+        reduction = max(4, out_channels // 16)
+        self.SE = SE(out_channels, reduction)
 
     def forward(self, x):
         residual = x
@@ -44,39 +45,40 @@ class FR_PDP_block(torch.nn.Module):
             x = x + residual
         return x
         
-class Bottleneck(nn.Module):
-    def __init__(self, in_channels, bottleneck_channels, out_channels):
-        super().__init__()
-        self.conv1 = nn.Conv2d(in_channels, bottleneck_channels, kernel_size=1)
-        self.bn1 = nn.BatchNorm2d(bottleneck_channels)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv2d(bottleneck_channels, out_channels, kernel_size=1)
-        self.bn2 = nn.BatchNorm2d(out_channels)
+# class Bottleneck(nn.Module):
+#     def __init__(self, in_channels, bottleneck_channels, out_channels):
+#         super().__init__()
+#         self.conv1 = nn.Conv2d(in_channels, bottleneck_channels, kernel_size=1)
+#         self.bn1 = nn.BatchNorm2d(bottleneck_channels)
+#         self.relu = nn.ReLU(inplace=True)
+#         self.conv2 = nn.Conv2d(bottleneck_channels, out_channels, kernel_size=1)
+#         self.bn2 = nn.BatchNorm2d(out_channels)
 
-    def forward(self, x):
-        out = self.bn1(self.conv1(x))
-        out = self.relu(out)
-        out = self.bn2(self.conv2(out))
-        return out
+#     def forward(self, x):
+#         out = self.bn1(self.conv1(x))
+#         out = self.relu(out)
+#         out = self.bn2(self.conv2(out))
+#         return out
 
-class SEBottleneckBlock(torch.nn.Module):
-    def __init__(self, in_channels, out_channels, bottleneck_channels, reduction=16):
-        super().__init__()
-        self.conv1 = conv1x1_block(in_channels, bottleneck_channels, activation="relu")
-        self.conv2 = conv3x3_block(bottleneck_channels, bottleneck_channels, activation="relu")
-        self.conv3 = conv1x1_block(bottleneck_channels, out_channels, activation=None)
-        self.se = SE(out_channels, reduction)  # Thêm SE
-        self.shortcut = conv1x1_block(in_channels, out_channels, activation=None)
-        self.relu = torch.nn.ReLU(inplace=True)
+# 63.37 tot nhat hien tai
+# class SEBottleneckBlock(torch.nn.Module):
+#     def __init__(self, in_channels, out_channels, bottleneck_channels, reduction=16):
+#         super().__init__()
+#         self.conv1 = conv1x1_block(in_channels, bottleneck_channels, activation="relu")
+#         self.conv2 = conv3x3_block(bottleneck_channels, bottleneck_channels, activation="relu")
+#         self.conv3 = conv1x1_block(bottleneck_channels, out_channels, activation=None)
+#         self.se = SE(out_channels, reduction)  # Thêm SE
+#         self.shortcut = conv1x1_block(in_channels, out_channels, activation=None)
+#         self.relu = torch.nn.ReLU(inplace=True)
 
-    def forward(self, x):
-        residual = self.shortcut(x)
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        x = self.se(x)  # Áp dụng attention
-        x = x + residual
-        return self.relu(x)
+#     def forward(self, x):
+#         residual = self.shortcut(x)
+#         x = self.conv1(x)
+#         x = self.conv2(x)
+#         x = self.conv3(x)
+#         x = self.se(x)  # Áp dụng attention
+#         x = x + residual
+#         return self.relu(x)
 
         
 class TickNet(torch.nn.Module):
@@ -111,10 +113,10 @@ class TickNet(torch.nn.Module):
             stage = torch.nn.Sequential()
             for unit_id, unit_channels in enumerate(stage_channels):
                 stride = strides[stage_id] if unit_id == 0 else 1  
-                if in_channels == 512 and unit_channels == 128:
-                    stage.add_module("unit{}".format(unit_id + 1), SEBottleneckBlock(in_channels=512, out_channels=128, bottleneck_channels=256, reduction=16))
-                else:
-                    stage.add_module("unit{}".format(unit_id + 1), FR_PDP_block(in_channels=in_channels, out_channels=unit_channels, stride=stride))
+                # if in_channels == 512 and unit_channels == 128:
+                #     stage.add_module("unit{}".format(unit_id + 1), SEBottleneckBlock(in_channels=512, out_channels=128, bottleneck_channels=256, reduction=16))
+                # else:
+                stage.add_module("unit{}".format(unit_id + 1), FR_PDP_block(in_channels=in_channels, out_channels=unit_channels, stride=stride))
                 in_channels = unit_channels
             self.backbone.add_module("stage{}".format(stage_id + 1), stage)
         self.final_conv_channels = 1024        
@@ -122,20 +124,31 @@ class TickNet(torch.nn.Module):
         self.backbone.add_module("global_pool", torch.nn.AdaptiveAvgPool2d(output_size=1))
         in_channels = self.final_conv_channels
         # classifier
-        self.classifier = Classifier(in_channels=in_channels, num_classes=num_classes)
-
+        # self.classifier = Classifier(in_channels=in_channels, num_classes=num_classes)
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=0.3),  # Add dropout
+            Classifier(in_channels=self.final_conv_channels, num_classes=num_classes)
+        )
         self.init_params()
 
     def init_params(self):
         # backbone
         for name, module in self.backbone.named_modules():
             if isinstance(module, torch.nn.Conv2d):
-                torch.nn.init.kaiming_uniform_(module.weight)
+                # torch.nn.init.kaiming_uniform_(module.weight)
+                torch.nn.init.kaiming_uniform_(module.weight, mode='fan_out', nonlinearity='relu')
                 if module.bias is not None:
                     torch.nn.init.constant_(module.bias, 0)
-
+            elif isinstance(module, nn.BatchNorm2d):
+                torch.nn.init.constant_(module.weight, 1)
+                torch.nn.init.constant_(module.bias, 0)
         # classifier
-        self.classifier.init_params()
+        # self.classifier.init_params()
+        for module in self.classifier.modules():
+            if isinstance(module, nn.Linear):
+                init.kaiming_uniform_(module.weight, mode='fan_out', nonlinearity='relu')
+                if module.bias is not None:
+                    init.constant_(module.bias, 0)
 
     def forward(self, x):
         x = self.backbone(x)
